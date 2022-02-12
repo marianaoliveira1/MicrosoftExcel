5094c55500bd4a8a2f918b0b104c7010f6a0c15b
Mariana Oliveira <marianaoliveirafernandes19@gmai.com>
2022-02-12 13:48:21 -0300

    atualizacao

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:syncfusion_flutter_xlsio/xlsio.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,8 +26,24 @@ class _HomePageState extends State<HomePage> {
 
   Future<void> createExcel() async {
     final Workbook workbook = Workbook();
+    final Worksheet sheet = workbook.worksheets[0];
+    sheet.getRangeByName('A1').setText('Hello World!');
     final List<int> bytes = workbook.saveAsStream();
     workbook.dispose();
 
+    if (kIsWeb) {
+      AnchorElement(
+          href:
+              'data:application/octet-stream;charset=utf-16le;base64,${base64.encode(bytes)}')
+        ..setAttribute('download', 'Output.xlsx')
+        ..click();
+    } else {
+      final String path = (await getApplicationSupportDirectory()).path;
+      final String fileName =
+          Platform.isWindows ? '$path\\Output.xlsx' : '$path/Output.xlsx';
+      final File file = File(fileName);
+      await file.writeAsBytes(bytes, flush: true);
+      OpenFile.open(fileName);
+    }
   }
 }
